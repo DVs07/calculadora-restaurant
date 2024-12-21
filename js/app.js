2d85650d2c0d69a2161f1e322e283be75f4b073a
DVs07 <aldopablo7@hotmail.com>
2024-12-20 23:46:30 -0300

    Se agrego el contador a cada plato y se los inyecta de forma dinamica al HTML, se muestra la mesa, cantidad, nombre del plato y el precio en el resumen de consumo.

--- js/app.js
+++ js/app.js
@@ -14,28 +14,31 @@ const btnGuardar = document.querySelector('#guardar-cliente');
 const formularioModal = document.querySelector('.modal-body form');
 btnGuardar.addEventListener('click', guardarCliente);
 
+const divContenido = document.querySelector('#resumen .contenido');
+
 function guardarCliente(){
     const mesa = document.querySelector('#mesa').value;
     const hora = document.querySelector('#hora').value;
 
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+
     // Validar
     const  camposVacios = [mesa, hora].some(campo => campo === '');
+
+    // Asignar datos del formulario al objeto cliente
+    cliente = {...cliente, mesa, hora};    
 
     if(camposVacios){
         imprimirAlerta('Hay campos vacios');        
     }else{
         console.log('No hay campos vacios');
     }
-
-    // Asignar datos del formulario al objeto cliente
-    cliente = {...cliente, mesa, hora};
-
-    // Ocultar modal
-    const modalFormulario = document.querySelector('#formulario');
-    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
-    modalBootstrap.hide();
+    
     formularioModal.reset();
-    // Mostar secciones
+
+    // Cierro modal y muestro secciones
+    cerrarModal(modalFormulario, modalBootstrap);
     mostrarSecciones();
 
     obtenerDatos();
@@ -94,12 +97,171 @@ function mostrarPlatos(platos){
         categoria.classList.add('col-md-3');
         categoria.textContent = categorias[plato.categoria];
 
+        const inputCantidad = document.createElement('input');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${plato.id}`;
+        inputCantidad.classList.add('form-control');
+
+        inputCantidad.onchange = function(){
+            const cantidad = parseInt(inputCantidad.value);
+            // console.log('Cantidad: ', cantidad);
+            
+            agregarPlato({...plato, cantidad});
+        }; 
+
+        const contendorInput = document.createElement('div');
+        contendorInput.classList.add('col-md-2');
+        
+        contendorInput.appendChild(inputCantidad);
+
         row.appendChild(nombre);
         row.appendChild(precio);
         row.appendChild(categoria);
+        row.appendChild(contendorInput);
 
         divPlatos.appendChild(row);
 
     });
     
+}
+
+function cerrarModal(modalElement, bootstrapModalInstance) {
+    bootstrapModalInstance.hide();
+    
+    modalElement.addEventListener('hidden.bs.modal', () => {
+    document.body.classList.remove('modal-open');
+     document.body.style.overflow = ''; // Restablecer overflow
+    
+    const backdrop = document.querySelector('.modal-backdrop');
+
+    if (backdrop) {
+        backdrop.remove();
+    }
+
+    modalElement.remove(); // Eliminar el modal del DOM
+    });
+}
+
+
+function agregarPlato(orden){
+    // console.log('ID: ', id);   
+    // console.log('Plato: ', orden);
+
+    let {pedido} = cliente; // Desestructuro el pedido
+
+    if(orden.cantidad > 0){
+        // console.log('Se agrego un plato'); 
+        // console.log(pedido.some(elemento => elemento.id === orden.id)); // Verifica si el plato ya esta en el pedido
+        
+        // Compruebo si el elemento ya existe en le array
+        if(pedido.some(elemento => elemento.id === orden.id)){
+            // El elemento ya existe
+            const pedidoActualizado = pedido.map(elemento => {
+                if(elemento.id === orden.id){
+                    elemento.cantidad = orden.cantidad;
+                }
+                return elemento;
+            })
+
+            // Actualizo el pedido
+            cliente.pedido = [...pedidoActualizado]; 
+
+        }else{
+            // El elemento no existe, lo agrego al array
+           cliente.pedido = [...pedido, orden]; // Se agrega el plato al pedido  
+        }
+    }else{
+        // Eliminar elemento cuando la cantidad sea 0
+        const resultado = pedido.filter(elemento => elemento.id !== orden.id);
+
+        cliente.pedido = [...resultado];
+    }
+
+    // console.log('Pedido: ', cliente.pedido);
+    // Mostrar el Resumen
+    limpiarHTML(divContenido);
+    actualizarResumen();
+
+}
+
+function actualizarResumen(){
+    // console.log('Actualizando resumen');
+
+    const divResumen = document.createElement('div');
+    divResumen.classList.add('col-md-6', 'card', 'py-2', 'px-3', 'shadow');
+
+    const mesa = document.createElement('p');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold')
+    
+    const mesaSpan = document.createElement('span');
+    mesaSpan.textContent = cliente.mesa;
+    mesaSpan.classList.add('fw-normal')
+
+    const hora = document.createElement('p');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold')
+
+    const horaSpan = document.createElement('span');
+    horaSpan.textContent = cliente.hora;
+
+    mesa.appendChild(mesaSpan);
+    hora.appendChild(horaSpan);
+
+    const heading = document.createElement('h3');
+    heading.classList.add('my-4d','text-center');
+    heading.textContent = 'Platos Consumidos';
+
+    const listaPlatos = document.createElement('ul');
+    listaPlatos.classList.add('list-group');
+
+    const {pedido} = cliente;
+    pedido.forEach(plato => {
+        const {nombre, cantidad, precio, id} = plato;
+
+        const listaPlato = document.createElement('li');
+        listaPlato.classList.add('list-group-item');
+
+        const nombrePlato = document.createElement('h4');
+        nombrePlato.classList.add('my-4');
+        nombrePlato.textContent = nombre;
+
+        const cantidadPlato = document.createElement('p');
+        cantidadPlato.classList.add('fw-bold');
+        cantidadPlato.textContent = `Cantidad: ${cantidad}`;
+
+        const precioPlato = document.createElement('p');
+        precioPlato.classList.add('fw-bold');
+        precioPlato.textContent = `Precio: ${precio}`;
+
+        const totalPlato = document.createElement('p'); 
+        totalPlato.classList.add('fw-bold');        
+        totalPlato.textContent = `Total: ${cantidad * precio}`;
+
+        listaPlato.appendChild(nombrePlato);
+        listaPlato.appendChild(cantidadPlato);
+        listaPlato.appendChild(precioPlato);
+        listaPlato.appendChild(totalPlato);
+
+        listaPlatos.appendChild(listaPlato);
+
+    })
+
+
+
+    divResumen.appendChild(mesa);
+    divResumen.appendChild(hora);
+    divResumen.appendChild(heading);
+    divResumen.appendChild(listaPlatos);
+
+    divContenido.appendChild(divResumen);
+    
+}
+
+function limpiarHTML(elemento){
+    while(elemento.firstChild){
+        elemento.removeChild(elemento.firstChild);
+    }
 }
